8cf44ddfe8119625efc741b753459fb6c316750a
owonikoko Ayomide <donaykoko@yahoo.com>
2024-04-05 11:35:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>  
 <html lang="en">      
-<head> 
+<head>  
   <link rel="stylesheet" href="grades.css"> 
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
