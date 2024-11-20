ca701bdc765bf31f20b7a3ac6ef5487f0eb6842a
owonikoko Ayomide <donaykoko@yahoo.com>
2024-11-20 23:34:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <title>Home</title>   
 </head>     
 <body>              
-  <div class="container">  
+  <div class="container">   
     <div class="all"> 
       <div class="head">
         <h1>GIGSTECH FIRST SEMESTER EXAMINATION RESULTS</h1> 
