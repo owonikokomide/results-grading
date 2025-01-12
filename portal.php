85100fe531a391330142717dc846f896ffdb21a9
owonikoko Ayomide <donaykoko@yahoo.com>
2025-01-12 16:55:13 +0100

    Update portal.php

--- portal.php
+++ portal.php
@@ -4,7 +4,7 @@
 <head>
   <link rel="stylesheet" href="grades.css">
   <meta charset="UTF-8">  
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
   <title>portal</title> 
 </head>    
 <body>     
